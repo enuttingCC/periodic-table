a48a7ca908a13f21ba0f446aa724387edbcda8af
Jordan Papaleo <papaleowebdev@gmail.com>
2015-08-30 20:50:05 -0700

    Let there be code

---
+++ src/api/index.js
@@ -0,0 +1,13 @@
+require('es6-promise').polyfill();
+
+import 'normalize.css';
+import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss';
+import 'angular';
+import Angular from 'angular';
+import Api from 'Api';
+
+
+
+
+
+document.write('wazzap');
